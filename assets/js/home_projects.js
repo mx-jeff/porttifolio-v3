62ab708ccf78735bef2a52732b6437e7dbed1ff4
Jeferson <mx,jeferson.12@gmail.com>
2021-01-22 13:15:37 -0300

    add projects section

--- assets/js/home_projects.js
+++ assets/js/home_projects.js
@@ -1,8 +1,28 @@
 import { GithubApi } from './api.js'
 
-export function projectsList(){
+export async function projectsList(){
     const container = document.querySelector('#project-list')
     // console.log(container)
     const github = new GithubApi()
-    github.repos().then(data => data.map(repo => console.log(repo)))
+    const githubData = await github.repos()
+    githubData.map(data => {
+        if(data.homepage){
+            // const link = document.createElement('a')
+            // link.href = data.homepage
+            // link.innerText = data.name
+            // link.classList.add('project-link')
+            // container.appendChild(link)
+
+            const project = document.createElement('div')
+            project.innerHTML = `
+                <h3>${data.name}</h3>
+                <p>${data.description}</p>
+                <div class="link-container">
+                    <a href="${data.homepage}">Visualizar projeto</a>
+                    <a href="${data.repo}">Repositorio</a>
+                </div>
+            `;
+            container.appendChild(project)
+        }
+    })
 }
--- assets/js/slick.js
+++ assets/js/slick.js
@@ -1,6 +1,44 @@
 export function mainSlick() {
     // Certificates slick
-    $('.certificates-slick').slick({
+    $('.slick-container').slick({
+        dots: true,
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+
+    $('.projects-slick').slick({
         dots: true,
         infinite: true,
         autoplay: true,
