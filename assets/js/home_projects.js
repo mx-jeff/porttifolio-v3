910b94aedf28356312836ff73a6d65b87e404a7f
Jeferson <mx,jeferson.12@gmail.com>
2021-01-29 23:04:29 -0300

    update repo url

--- assets/js/home_projects.js
+++ assets/js/home_projects.js
@@ -7,19 +7,13 @@ export async function projectsList(){
     const githubData = await github.repos()
     githubData.map(data => {
         if(data.homepage){
-            // const link = document.createElement('a')
-            // link.href = data.homepage
-            // link.innerText = data.name
-            // link.classList.add('project-link')
-            // container.appendChild(link)
-
             const project = document.createElement('div')
             project.innerHTML = `
                 <h3>${data.name}</h3>
                 <p>${data.description}</p>
                 <div class="link-container">
                     <a href="${data.homepage}">Visualizar projeto</a>
-                    <a href="${data.repo}">Repositorio</a>
+                    <a href="${data.html_url}">Repositorio</a>
                 </div>
             `;
             project.classList.add('project')
