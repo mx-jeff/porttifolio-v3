564c13831ad3a3a254d27eb80a465a08f2c3e8fe
Jeff <mx.jeferson.12@gmail.com>
2021-12-07 11:28:24 -0300

    update error handling

--- assets/js/location.js
+++ assets/js/location.js
@@ -14,26 +14,28 @@ export function getLocation() {
     enBody.style.display = "none"
     bodyPt.style.display = "none"
 
-    try{
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(async (position) => {
-                const { latitude, longitude } = position.coords
-                const apiKey = "b6219e522f57eb2c8547d613a9b0224a"
-                const countryFindUrl = `http://api.positionstack.com/v1/reverse?access_key=${apiKey}&query=${latitude},${longitude}`
-    
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(async (position) => {
+            const { latitude, longitude } = position.coords
+            const apiKey = "b6219e522f57eb2c8547d613a9b0224a"
+            const countryFindUrl = `http://api.positionstack.com/v1/reverse?access_key=${apiKey}&query=${latitude},${longitude}`
+
+            try{
                 const response = await fetch(countryFindUrl)
                 if(response.status != 200) return
                 const data = await response.json()
                 const { country } = data.data[0]
                 displayBodyLang(country, enBody, bodyPt)
-            })
-        } else {
-            console.error("Geolocation is not supported by this browser.")
-            return
-        }
-    } catch(e){
-        enBody.style.display = "block"
-        console.error(e)
+
+            } catch(e){
+                enBody.style.display = "block"
+                console.log(e)
+            }
+        })
+    } else {
+        console.error("Geolocation is not supported by this browser.")
+        return
     }
     
 }
