ec2223f608c7f7792806164fff6277f1302a9b5f
Jeff <mx.jeferson.12@gmail.com>
2021-12-07 16:29:26 -0300

    en navbar

--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,4 +1,7 @@
 export function loadMaterialize(){
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+    var en_elems = document.querySelectorAll('.en_sidenav');
+    var en_instances = M.Sidenav.init(en_elems, {});
 }
