8036e340b03390d415926eb219340a8afc65dcef
Jeff <mx.jeferson.12@gmail.com>
2021-12-08 11:54:54 -0300

    update geolocation

--- assets/js/location.js
+++ assets/js/location.js
@@ -18,19 +18,23 @@ export function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(async (position) => {
             const { latitude, longitude } = position.coords
-            const apiKey = "b6219e522f57eb2c8547d613a9b0224a"
-            const countryFindUrl = `http://api.positionstack.com/v1/reverse?access_key=${apiKey}&query=${latitude},${longitude}`
+            // const apiKey = "b6219e522f57eb2c8547d613a9b0224a"
+            const geocodeAPikey = "&auth=260344906473753239972x1557"
+            // const countryFindUrl = `http://api.positionstack.com/v1/reverse?access_key=${apiKey}&query=${latitude},${longitude}`
 
+            const countryFindUrl = `https://geocode.xyz/${latitude},${longitude}?json=1${geocodeAPikey}`
             try{
                 const response = await fetch(countryFindUrl)
                 if(response.status != 200) return
                 const data = await response.json()
-                const { country } = data.data[0]
+                const { country } = data
+                console.log(country)
                 displayBodyLang(country, enBody, bodyPt)
 
             } catch(e){
-                bodyPt.style.display = "block"
-                console.log(e)
+                enBody.style.display = "block"
+                // bodyPt.style.display = "block"
+                console.error(e)
             }
         })
     } else {
--- assets/js/materialize.js
+++ assets/js/materialize.js
@@ -1,7 +1,4 @@
 export function loadMaterialize(){
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
-
-    var en_elems = document.querySelectorAll('.en_sidenav');
-    var en_instances = M.Sidenav.init(en_elems, {});
 }
